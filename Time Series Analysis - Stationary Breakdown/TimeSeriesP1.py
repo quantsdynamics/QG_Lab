48723bd562559c0d7c58aafe12e9e46ae195d33d
quantsdynamics <70588187+quantsdynamics@users.noreply.github.com>
2020-11-27 16:46:36 +0700

    Add files via upload

---
+++ Time Series Analysis - Stationary Breakdown/TimeSeriesP1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ronnawat, CQF ,CFAII ,AFPT
+
+"""
+
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def tsplot(y, lags=None, figsize=(15, 10), style='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):    
+        fig = plt.figure(figsize=figsize)
+        #mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+        
+        y.plot(ax=ts_ax)
+        ts_ax.set_title('Time Series Analysis Plots')
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.05)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.05)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')        
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return 
+
+df = pd.read_csv('C:/Users/Ronnawat/Desktop/Trading with Python/ลงทุนเป็นระบบ Finlab/TimeSeries/Part1/BTC-USD.csv', index_col=0, parse_dates=True)
+
+# First difference of SPX prices
+_ = tsplot(np.diff(df['Adj Close']), lags=30)
